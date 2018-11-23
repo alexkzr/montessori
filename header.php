1eb666a79642d50714dcdf41b63f2b2f9941dfd6
Alex Kozyrev <alexkozyrev82@gmail.com>
2018-11-23 20:42:24 +0300

    teachers.html ready, almost adaptive

---
+++ header.php
@@ -0,0 +1,63 @@
+ <!doctype html>
+<html lang="ru">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap-grid.css">
+  <link rel="stylesheet" href="css/slick-theme.css">
+  <link rel="stylesheet" href="css/slick.css">
+  <link rel="stylesheet" href="css/lightbox.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Международная школа Монтессори</title>
+</head>
+
+<body>
+ <header class="header">
+      <div class="header-top">
+        <div class="container">
+          <div class="logo"></div>
+          <a href="tel:+79219935550" class="header-phone">+7(921) 993-5550 </a>
+          <a href="mailto:info@montessori-petersburg.com" class="header-email">info@montessori-petersburg.com</a>
+          <a href="#" class="full" id="full">Полная версия сайта</a>
+          <a href="" class="header-lang">
+            <a href="#" class="header-lang__ru">Rus</a>
+            <a href="#" class="header-lang__en">Eng</a>
+          </a>
+        </div>
+      </div>
+      <!-- /.header-top -->
+      <nav class="header-bottom">
+        <div class="container">
+          <ul>
+            <li><a href="#">главная</a></li>
+            <li><a href="#">программы</a></li>
+            <li><a href="#">преподаватели</a></li>
+            <li><a href="#">новости и календарь</a></li>
+            <li><a href="#">условия</a></li>
+            <li><a href="#">контакты</a></li>
+            <li class="hidden"><a href="#">сведения об организации</a>
+              <ul class="secondary">
+                <li><a href="#">Основные сведения</a></li>
+                <li><a href="#">Структура и органы управления образовательной организацией</a></li>
+                <li><a href="#">Документы</a></li>
+                <li><a href="#">Образование</a></li>
+                <li><a href="#">Образовательные стандарты</a></li>
+                <li><a href="#">Руководство. Педагогический (научно-педагогический) состав</a></li>
+                <li><a href="#">Материально-техническое обеспечение и оснащенность образовательного процесса</a></li>
+                <li><a href="#">Стипендии и иные виды материальной поддержки</a></li>
+                <li><a href="#">Платные образовательные услуги</a></li>
+                <li><a href="#">Финансово-хозяйственная деятельность</a></li>
+                <li><a href="#">Вакантные места для приема (перевода)</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <!-- /.header-bottom -->
+    </header>
+    <!-- /.header -->
